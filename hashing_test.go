5b4a82cbb2f917677fe05b694a3d1291ea504c79
Maximiliano Ancillotti <maxi.ancillotti@gmail.com>
2022-07-25 21:56:26 -0300

    adding a few more test cases

--- hashing_test.go
+++ hashing_test.go
@@ -18,3 +18,54 @@ func TestHashing(t *testing.T) {
 	}
 	t.Log("Hash validation OK")
 }
+
+func TestHashing2(t *testing.T) {
+
+	pw := "rW&r#`@nFRw]2@reVU+~h0q pn5}@`_TQkwf6\"H:f,`24&=gWi~ag%8GC/z#:+Gg"
+	salt := "N|pf2dO:CwH\\2hjgH?dc(( ^agdh;4  "
+
+	t.Logf("randpw: '%s'", pw)
+	t.Logf("randsalt: '%s'", salt)
+
+	storedHashpw := "$2a$10$eIqiLuuijq/kZo59gdq99eenwxhiXYVW1oTzkoYXzDHhqYM0C.3K2"
+	t.Logf("storedHashpw: '%s'", storedHashpw)
+
+	validationErr := ValidateHashPw(pw, salt, storedHashpw)
+	if validationErr != nil {
+		t.Error("Stored hash validation failed:", validationErr)
+		return
+	}
+
+	newHashedpw, _ := HashPw(pw, salt)
+	t.Logf("storedHashpw: '%s'", newHashedpw)
+
+	validationErr = ValidateHashPw(pw, salt, newHashedpw)
+	if validationErr != nil {
+		t.Error("New hash validation failed:", validationErr)
+		return
+	}
+
+	t.Log("Hash validation OK")
+}
+
+func TestHashingWithGenerator(t *testing.T) {
+	pwLen := 64
+	saltLen := 32
+
+	pw := RandASCIIString(pwLen)
+	salt := RandASCIIString(saltLen)
+
+	t.Logf("randpw: '%s'", pw)
+	t.Logf("randsalt: '%s'", salt)
+
+	hashed, _ := HashPw(pw, salt)
+	t.Logf("storedHashpw: '%s'", hashed)
+
+	validationErr := ValidateHashPw(pw, salt, hashed)
+	if validationErr != nil {
+		t.Error("New hash validation failed:", hashed)
+		return
+	}
+
+	t.Log("Hash validation OK")
+}
